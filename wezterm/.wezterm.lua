538c4522536d7ad34a769b2c20862c12e501073c
CloudyFluke <cloudyfluke@gmail.com>
2025-11-25 20:14:49 -0600

    wezterm disable super f and add ssh

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -23,5 +23,30 @@ config.background = {
 	},
 }
 
+-- NOTE: Disable because it conflicts with Hyprland binding for fullscreen
+config.keys = {
+	{
+		key = "f",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "f",
+		mods = "SUPER",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+config.ssh_domains = {
+	{
+		-- This name identifies the domain
+		name = "home-server",
+		-- The hostname or address to connect to. Will be used to match settings
+		-- from your ssh config file
+		remote_address = "home-server",
+		-- The username to use on the remote host
+	},
+}
+
 -- Finally, return the configuration to wezterm:
 return config
