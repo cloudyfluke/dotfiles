eddccd6ec7aa8ed07451ae72a5c33a59792a2ea8
CloudyFluke <cloudyfluke@gmail.com>
2026-01-31 10:20:31 -0600

    finally

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,30 @@
 return {
-  {
+  { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    branch = "main",
-    build = ":TSUpdate",
-
     config = function()
-      require("nvim-treesitter.config").setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-
-        ensure_installed = {
-          "bash",
-          "c",
-          "diff",
-          "lua",
-          "luadoc",
-          "markdown",
-          "markdown_inline",
-          "query",
-          "vim",
-          "vimdoc",
-          "go",
-          "odin",
-          "zig",
-          "python",
-          "printf",
-          "ninja",
-          "rst",
-        },
-        sync_install = true,
-        auto_install = true,
-        indent = { enable = true },
-        highlight = {
-          enable = true,
-          disable = {},
-          additional_vim_regex_highlighting = false,
-        },
+      local filetypes = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "vim",
+        "vimdoc",
+        "go",
+        "odin",
+        "python",
+      }
+      require("nvim-treesitter").install(filetypes)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function()
+          vim.treesitter.start()
+        end,
       })
     end,
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter-textobjects",
-  --   after = "nvim-treesitter",
-  --   requires = "nvim-treesitter/nvim-treesitter",
-  -- },
 }
